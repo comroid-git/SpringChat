323da0d9e0e9c5e64100fb74851e5af7e5679fc6
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-03 13:31:27 +0100

    some stuff

--- src/main/java/org/comroid/springchat/SpringChatApplication.java
+++ src/main/java/org/comroid/springchat/SpringChatApplication.java
@@ -1,5 +1,7 @@
 package org.comroid.springchat;
 
+import org.comroid.api.ContentParser;
+import org.comroid.api.io.FileHandle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,6 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"org.comroid.springchat.config", "org.comroid.springchat.controller"})
 @EntityScan(basePackages = {"org.comroid.springchat.entity"})
 public class SpringChatApplication {
+    public static final FileHandle DATA_DIR = new FileHandle("/srv/chat/", true);
+    public static final FileHandle OAUTH_SECRET_FILE = DATA_DIR.createSubFile("oauth_secret.txt");
+
     public static void main(String[] args) {
         SpringApplication.run(SpringChatApplication.class, args);
     }
---
+++ src/main/java/org/comroid/springchat/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package org.comroid.springchat.config;
+
+import org.comroid.springchat.SpringChatApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        security.oauth2ResourceServer()
+                .jwt();
+        return security.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        // todo this seems hella wrong
+        return new InMemoryClientRegistrationRepository(
+                ClientRegistration.withRegistrationId("chat-client")
+                        .clientId("chat-client")
+                        .clientSecret(SpringChatApplication.OAUTH_SECRET_FILE.getContent())
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                        .scope("username")
+                        .authorizationUri("https://auth.comroid.org/authorized")
+                        .tokenUri("https://auth.comroid.org/oauth2/token")
+                        .userInfoUri("https://auth.comroid.org/oauth2/userinfo")
+                        .clientName("SpringChat")
+                        .build()
+        );
+    }
+}
