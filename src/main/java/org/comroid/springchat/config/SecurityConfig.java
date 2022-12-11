f749a0997bbcd970744783ffdccb72bc052b93d8
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-11 18:59:41 +0100

    this is so weird

--- src/main/java/org/comroid/springchat/config/SecurityConfig.java
+++ src/main/java/org/comroid/springchat/config/SecurityConfig.java
@@ -7,11 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,8 +30,8 @@ import java.util.List;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
-        security.oauth2ResourceServer()
-                .jwt();
+        security.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        ;
         return security.build();
     }
 
@@ -39,11 +46,34 @@ public class SecurityConfig {
                         .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                         .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
                         .scope("username")
-                        .authorizationUri("https://auth.comroid.org/authorized")
-                        .tokenUri("https://auth.comroid.org/oauth2/token")
-                        .userInfoUri("https://auth.comroid.org/oauth2/userinfo")
+                        .authorizationUri("http://localhost:42000/oauth2/authorize")
+                        .tokenUri("http://localhost:42000/oauth2/token")
+                        .userInfoUri("http://localhost:42000/oauth2/userinfo")
                         .clientName("SpringChat")
                         .build()
         );
     }
+    /*
+    @Bean
+    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
+                ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
+                        .clientCredentials()
+                        .password()
+                        .build();
+
+        DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultReactiveOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
+     */
 }
